c93f0c8b8c09af22070cd60a825bfede082f58fe
KaiqueT. <103083508+KaciqueT@users.noreply.github.com>
2024-12-11 17:21:35 -0300

    Delete Estoque directory

--- Estoque/Utilitarios/Utilitarios.js
+++
@@ -1,9 +0,0 @@
-const maiorZIndex = () => {
-    return Math.max(
-        ...Array.from(
-            document.querySelectorAll('body *'),
-            (el) => {parseFloat(window.getComputedStyle(el).zIndex)},
-        ).filter((zIndex) => {!Number.isNaN(zIndex)}),
-        0,
-    )
-}
